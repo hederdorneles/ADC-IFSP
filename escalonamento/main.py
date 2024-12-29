6afa1f9ca3d4f99d8e778c4246247823629c641d
Heder Dorneles <hederdorneles@users.noreply.github.com>
2024-12-28 21:26:24 -0300

    Update main.py

--- escalonamento/main.py
+++ escalonamento/main.py
@@ -17,7 +17,6 @@ class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('Simulador de Escalonamento de Processos')
-        
         self.setGeometry(100, 100, 800, 400)
         
         # Layouts
@@ -26,133 +25,100 @@ class MainWindow(QWidget):
         
         # Cria a barra de menu
         self.create_menu_bar()
-
-        # Adiciona a barra de menu ao layout
         main_layout.setMenuBar(self.menu_bar)
         
         # Campo de texto para visualizar o nome do processo atual
         self.process_name_label = QLabel('Processo Atual: A')
         main_layout.addWidget(self.process_name_label)
         
-        # Campo de entrada para tempo de chegada
+        # Campos de entrada
         self.arrival_input = QLineEdit()
         self.arrival_input.setPlaceholderText('Tempo de Chegada')
         input_layout.addWidget(self.arrival_input)
         
-        # Campo de entrada para tempo de execução
         self.execution_input = QLineEdit()
         self.execution_input.setPlaceholderText('Tempo de Execução')
         input_layout.addWidget(self.execution_input)
         
-        # ComboBox para escolher o algoritmo de escalonamento
         self.algorithm_combo = QComboBox()
-        self.algorithm_combo.addItem("FCFS")
-        self.algorithm_combo.addItem("SPN")
-        self.algorithm_combo.addItem("SRT")
-        self.algorithm_combo.addItem("Round Robin")
+        self.algorithm_combo.addItems(["FCFS", "SPN", "SRT", "Round Robin"])
         input_layout.addWidget(self.algorithm_combo)
 
-        # Campo de entrada para o quantum (Round Robin)
         self.quantum_input = QLineEdit()
         self.quantum_input.setPlaceholderText('Quantum')
         input_layout.addWidget(self.quantum_input)
         
         main_layout.addLayout(input_layout)
         
-        # Botão para adicionar processo à tabela
+        # Botões
         self.add_button = QPushButton('Adicionar Processo')
         self.add_button.clicked.connect(self.add_process)
         main_layout.addWidget(self.add_button)
         
-        # Tabela para exibir os processos
+        self.execute_button = QPushButton('Executar')
+        self.execute_button.clicked.connect(self.execute_scheduling)
+        main_layout.addWidget(self.execute_button)
+        
+        self.clear_button = QPushButton('Limpar')
+        self.clear_button.clicked.connect(self.clear_simulation)
+        main_layout.addWidget(self.clear_button)
+        
+        # Tabela de processos
         self.process_table = QTableWidget()
         self.process_table.setColumnCount(3)
         self.process_table.setHorizontalHeaderLabels(['Nome', 'Tempo de Chegada', 'Tempo de Execução'])
         main_layout.addWidget(self.process_table)
         
-        # Botão para executar o escalonamento
-        self.execute_button = QPushButton('Executar')
-        self.execute_button.clicked.connect(self.execute_scheduling)
-        main_layout.addWidget(self.execute_button)
-        
-        # Botão para limpar a simulação
-        self.clear_button = QPushButton('Limpar')
-        self.clear_button.clicked.connect(self.clear_simulation)
-        main_layout.addWidget(self.clear_button)
-        
-        # Figura para o gráfico de barras
+        # Gráfico de barras
         self.bar_chart = plt.figure()
         self.bar_chart_canvas = FigureCanvas(self.bar_chart)
         main_layout.addWidget(self.bar_chart_canvas)
         
         self.setLayout(main_layout)
-        
-        # Variável para controlar o nome do próximo processo
         self.next_process_name = 'A'
         
     def create_menu_bar(self):
-        # Cria a barra de menu
         self.menu_bar = QMenuBar()
-
-        # Cria o menu "Arquivo" e adiciona a ação "Sair"
         file_menu = self.menu_bar.addMenu('Arquivo')
         exit_action = QAction('Sair', self)
         exit_action.setShortcut('Esc')
         exit_action.triggered.connect(self.close)
         file_menu.addAction(exit_action)
-
-        # Cria o menu "Ajuda" e adiciona a ação "Sobre"
+        
         help_menu = self.menu_bar.addMenu('Ajuda')
         about_action = QAction('Sobre', self)
         about_action.triggered.connect(self.show_about_dialog)
         help_menu.addAction(about_action)
         
     def show_about_dialog(self):
-        about_text = "Escalonador de processos 1.0\nAutor: Heder Dorneles Soares\n \
-        Curso de Análise e Desenvolvimento de Sistemas."
+        about_text = "Escalonador de processos 1.0\nAutor: Heder Dorneles Soares\nCurso de Análise e Desenvolvimento de Sistemas."
         QMessageBox.about(self, 'Sobre', about_text)
         
     def add_process(self):
-        # Obter os dados do processo dos campos de entrada
         name = self.next_process_name
         arrival_time = self.arrival_input.text()
         execution_time = self.execution_input.text()
         
-        # Inserir os dados na tabela de processos
         row_position = self.process_table.rowCount()
         self.process_table.insertRow(row_position)
         self.process_table.setItem(row_position, 0, QTableWidgetItem(name))
         self.process_table.setItem(row_position, 1, QTableWidgetItem(arrival_time))
         self.process_table.setItem(row_position, 2, QTableWidgetItem(execution_time))
         
-        # Atualizar o nome do próximo processo
         self.next_process_name = chr(ord(self.next_process_name) + 1)
-        
-        # Atualizar o campo de texto para visualizar o nome do processo atual
         self.process_name_label.setText(f'Processo Atual: {self.next_process_name}')
-
-        # Limpar os campos de entrada
+        
         self.arrival_input.clear()
         self.execution_input.clear()
         
     def execute_scheduling(self):
-        algorithm = self.algorithm_combo.currentText()  # Obtém o algoritmo selecionado
+        algorithm = self.algorithm_combo.currentText()
         
-        # # Remove o campo de entrada para o quantum caso o algoritmo selecionado não seja Round Robin
-        # if algorithm != "Round Robin" and hasattr(self, 'quantum_input'):
-        #     self.quantum_input.deleteLater()
-        #     del self.quantum_input
+        if algorithm == "Round Robin" and not self.quantum_input.text():
+            return
         
-        # Adiciona o campo de entrada para o quantum caso o algoritmo selecionado seja Round Robin
-        if algorithm == "Round Robin" and not hasattr(self, 'quantum_input'):
-            self.quantum_input = QLineEdit()
-            self.quantum_input.setPlaceholderText('Quantum')
-            self.input_layout.addWidget(self.quantum_input)
-        
-        # Limpa o gráfico antes de executar o novo escalonador
         self.bar_chart.clear()
         
-        # Execução do escalonamento de acordo com o algoritmo escolhido
         if algorithm == "FCFS":
             execution_steps = self.execute_fcfs()
         elif algorithm == "SPN":
@@ -160,57 +126,32 @@ class MainWindow(QWidget):
         elif algorithm == "SRT":
             execution_steps = self.execute_srt()
         elif algorithm == "Round Robin":
-            if not hasattr(self, 'quantum_input'):
-                # Se o usuário não especificou o quantum, não podemos continuar
-                return
             quantum = int(self.quantum_input.text())
             execution_steps = self.execute_round_robin(quantum)
         
-        # Atualiza o gráfico de barras com os processos em execução em cada passo de tempo
         self.update_bar_chart(execution_steps)
 
     def execute_fcfs(self):
-        # Obtém os dados dos processos da tabela de entrada
         processes = self.get_processes_data()
-        
-        # Ordena os processos pelo tempo de chegada
         processes.sort(key=lambda x: x[1])
-        
-        # Executa o escalonamento FCFS
         return self.run_scheduling(processes)
         
     def execute_spn(self):
-        # Obtém os dados dos processos da tabela de entrada
         processes = self.get_processes_data()
-        
-        # Ordena os processos pelo tempo de chegada
         processes.sort(key=lambda x: x[1])
-        
-        # Executa o escalonamento SPN
         return self.run_scheduling(processes, shortest_remaining_time=True)
         
     def execute_srt(self):
-        # Obtém os dados dos processos da tabela de entrada
         processes = self.get_processes_data()
-        
-        # Executa o escalonamento SRT
         return self.run_scheduling(processes, shortest_remaining_time=True)
         
     def execute_round_robin(self, quantum):
-        # Obtém os dados dos processos da tabela de entrada
         processes = self.get_processes_data()
-        
-        # Calcula o tempo total de execução
         total_execution_time = sum(process[2] for process in processes)
-        
-        # Executa o escalonamento Round Robin
         return self.run_round_robin(processes, total_execution_time, quantum)
     
     def run_scheduling(self, processes, shortest_remaining_time=False):
-        # Cria uma lista para armazenar os processos em execução a cada passo de tempo
         execution_steps = [[] for _ in range(10)]
-        
-        # Executa o escalonamento
         current_time = 0
         while processes:
             available_processes = [process for process in processes if process[1] <= current_time]
@@ -218,7 +159,7 @@ class MainWindow(QWidget):
                 if shortest_remaining_time:
                     shortest_process = min(available_processes, key=lambda x: x[2])
                 else:
-                    shortest_process = available_processes[0]  # First come, first served
+                    shortest_process = available_processes[0]
                 process_name, arrival_time, execution_time = shortest_process
                 processes.remove(shortest_process)
                 for _ in range(execution_time):
@@ -226,78 +167,67 @@ class MainWindow(QWidget):
                     current_time += 1
             else:
                 current_time += 1
-        
         return execution_steps
 
     def run_round_robin(self, processes, total_execution_time, quantum):
-        # Cria uma lista para armazenar os processos em execução a cada passo de tempo
         execution_steps = [[] for _ in range(total_execution_time)]
-        
-        # Copia dos processos para evitar modificar a lista original
         processes = processes.copy()
-        
-        # Executa o escalonamento Round Robin
         current_time = 0
         while processes:
-            executed_any_process = False  # Variável para verificar se algum processo foi executado neste passo de tempo
-            for process in list(processes):  # Itera sobre uma cópia da lista de processos
+            executed_any_process = False
+            for process in list(processes):
                 process_name, arrival_time, execution_time = process
                 if arrival_time <= current_time and execution_time > 0:
                     executed_any_process = True
-                    for _ in range(min(execution_time, quantum)):
+                    time_slice = min(execution_time, quantum)
+                    for _ in range(time_slice):
                         if current_time < total_execution_time:
                             execution_steps[current_time].append(process_name)
-                        execution_time -= 1
                         current_time += 1
+                    execution_time -= time_slice
                     if execution_time <= 0:
                         processes.remove(process)
+                    else:
+                        idx = processes.index(process)
+                        processes[idx] = (process_name, arrival_time, execution_time)
             if not executed_any_process:
                 current_time += 1
-            if current_time >= total_execution_time:  # Verifica se ultrapassamos o tempo total de execução
+            if current_time >= total_execution_time:
                 break
-                    
         return execution_steps
 
     def update_bar_chart(self, execution_steps):
         ax = self.bar_chart.add_subplot(111)
-        ax.clear()  # Limpa o gráfico anterior
-        
-        # Extrai os nomes dos processos
+        ax.clear()
         process_names = sorted({process for step in execution_steps for process in step}, reverse=True)
-        
-        # Define os eixos
         ax.set_yticks(range(len(process_names)))
         ax.set_yticklabels(process_names)
+        ax.set_xticks(range(len(execution_steps) + 1))
         ax.set_xlabel('Tempo')
         ax.set_ylabel('Processos')
         ax.set_title('Execução dos Processos')
-        
-        # Cria uma paleta de cores para os processos
+        ax.grid(which='both', linestyle='--', linewidth=0.5, color='gray', alpha=0.7)
         colors = plt.cm.viridis(np.linspace(0, 1, len(process_names)))
         color_dict = {process: color for process, color in zip(process_names, colors)}
         
-        # Plota as barras de execução
-        for time, step in enumerate(execution_steps):
-            for i, process in enumerate(process_names):
-                if process in step:
-                    ax.bar(time, 1, bottom=len(process_names) - i - 1, align='center', color=color_dict[process])
+        # Ajusta a altura dos intervalos de dados do eixo y para serem iguais
+        bar_height = 1 / len(process_names)
         
-        # Redesenha o gráfico
+        for time, step in enumerate(execution_steps):
+            for process in step:
+                process_index = process_names.index(process)
+                ax.bar(time + 0.5, bar_height, bottom=process_index * bar_height, align='center', width=1, color=color_dict[process])
+        
+        ax.set_xlim(0, len(execution_steps))
+        ax.set_ylim(0, 1)
         self.bar_chart_canvas.draw()
 
     def clear_simulation(self):
-        # Limpa a tabela de processos
         self.process_table.clearContents()
         self.process_table.setRowCount(0)
-        
-        # Limpa o gráfico de barras
         self.bar_chart.clear()
         self.bar_chart_canvas.draw()
-        
-        # Reinicia o nome do próximo processo
         self.next_process_name = 'A'
-        
-        # Reinicia o texto do processo atual
         self.process_name_label.setText('Processo Atual: A')
         
     def get_processes_data(self):
